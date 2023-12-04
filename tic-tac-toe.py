7270e30e630fe120badd1cbf469a02004716cef5
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-12-04 14:25:59 +0100

    Création de la boucle de jeu et de la surface

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -8,13 +8,19 @@ Beginning of creation on Mon Dec 4 2023
 """
 
 #Importation de fonctions externes (librairies) :
-    
+
+import pygame    
     
 #Définition locale de fonctions : 
   
 
 #Déclaration des variables : 
-
-
+surface = pygame.display.set_mode((800,600))
+run = True
 #Corps principal du programme : 
-
+                                # Boucle de jeu
+while run :
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+pygame.quit()
