9f2844e186a9cd67bce17dfdcaaf8a60537c9c72
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-12-04 11:44:00 +0100

    Création du dossier et du fichier .py

---
+++ tic-tac-toe.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Beginning of creation on Mon Dec 4 2023
+
+@author: Mazard Pierre
+
+#                                  Tic-Tac-Toe
+"""
+
+#Importation de fonctions externes (librairies) :
+    
+    
+#Définition locale de fonctions : 
+  
+
+#Déclaration des variables : 
+
+
+#Corps principal du programme : 
+
