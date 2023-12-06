b6c533771a7732afdd28bc347ba4d33aa320fa29
Drazam <146709444+pierre-mazard@users.noreply.github.com>
2023-12-06 14:48:02 +0100

    Ebauche page de création des joueurs pour partie locale à 2 joueurs : ===> Création de deux rectangles (rouge et vert) dans lesquels s'afficheront les deux joeurs inscrits. =======> Reste à créer une zone de saisie dans laquelle est demandé d'inscrir le nom des deux joeurs qui une fois validé s'affichera dans les rectangle correspondants

--- main_tic_tac_toe/game_tic_tac_toe/two_player_local_game/creation_player.py
+++ main_tic_tac_toe/game_tic_tac_toe/two_player_local_game/creation_player.py
@@ -8,22 +8,44 @@ Created on Wed Dec  6  2023
 """
 
 #Importation de fonctions externes (librairies) :
+import pygame
 
-import pygame    
+# Initialisation de Pygame
+pygame.init()
 
-pygame.init()    
-
-
-#Définition locale de fonctions : 
-
-
-
-#Déclaration des variables : 
+# Dimensions de la fenêtre
+width_screen, height_screen = 800, 600
 
 
-#Corps principal du programme : 
+# Création de la fenêtre
+screen = pygame.display.set_mode((width_screen, height_screen))
+pygame.display.set_caption("Tic Tac Toe - Création des joueur")
+white = (255, 255, 255)
+screen.fill(white)
 
-pygame.display.set_caption("Tic Tac Toe - Partie locale à deux joueurs")
 
+# Coordonnées et dimensions des rectangles verts
+rect1 = pygame.Rect(100, 150, 250, 100)
+rect2 = pygame.Rect(500, 150, 250, 100)
+
+# Boucle principale
+continuer = True
+while continuer:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            continuer = False
+
+    # Dessin des rectangles verts
+    pygame.draw.rect(screen, (180, 0, 0), rect1)
+    pygame.draw.rect(screen, (0, 180, 0), rect2)
+
+    # Rafraîchissement de l'écran
+    pygame.display.flip()
+
+# Fermeture propre de Pygame
 pygame.quit()
-        
+
+
+
+
+
